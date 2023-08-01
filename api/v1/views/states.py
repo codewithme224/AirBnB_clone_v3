08bd435d213125f236dbb647b50ac9d7ee4461b5
codewithme224 <codewithme224@gmail.com>
2023-07-31 17:53:31 -0700

    Task updated

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,74 +1,72 @@
 #!/usr/bin/python3
-"""objects that handles all default RESTFul API actions"""
-
+"""states"""
 from api.v1.views import app_views
 from flask import jsonify, abort, request
 from models import storage
 from models.state import State
-from flasgger import Swagger, swag_from
+from datetime import datetime
+import uuid
 
 
-@app_views.route('/states', methods=['GET'])
-@swag_from('swagger_yaml/states_no_id.yml', methods=['GET'])
-def get_states():
-    """ Method to return all states """
-    states = storage.all("State")
-    states_list = []
-    for state in states.values():
-        states_list.append(state.to_dict())
-    return jsonify(states_list)
+@app_views.route('/states/', methods=['GET'])
+def list_states():
+    '''Retrieves a list of all State objects'''
+    list_states = [obj.to_dict() for obj in storage.all("State").values()]
+    return jsonify(list_states)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'])
-@swag_from('swagger_yaml/states_no_id.yml', methods=['GET'])
 def get_state(state_id):
-    """ Method to return a state """
-    state = storage.get("State", state_id)
-    if state is None:
+    '''Retrieves a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
-    return jsonify(state.to_dict())
+    return jsonify(state_obj[0])
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'])
-@swag_from('swagger_yaml/states_no_id.yml', methods=['DELETE'])
 def delete_state(state_id):
-    """ Method to delete a state """
-    state = storage.get("State", state_id)
-    if state is None:
+    '''Deletes a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
-    state.delete()
-    storage.save()
+    state_obj.remove(state_obj[0])
+    for obj in all_states:
+        if obj.id == state_id:
+            storage.delete(obj)
+            storage.save()
     return jsonify({}), 200
 
 
-@app_views.route('/states', methods=['POST'])
-@swag_from('swagger_yaml/states_no_id.yml', methods=['POST'])
+@app_views.route('/states/', methods=['POST'])
 def create_state():
-    """ Method to create a state """
+    '''Creates a State'''
     if not request.get_json():
-        abort(400, "Not a JSON")
-    if "name" not in request.get_json():
-        abort(400, "Missing name")
-    state = State(**request.get_json())
-    state.save()
-    return jsonify(state.to_dict()), 201
+        abort(400, 'Not a JSON')
+    if 'name' not in request.get_json():
+        abort(400, 'Missing name')
+    states = []
+    new_state = State(name=request.json['name'])
+    storage.new(new_state)
+    storage.save()
+    states.append(new_state.to_dict())
+    return jsonify(states[0]), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'])
-@swag_from('swagger_yaml/states_no_id.yml', methods=['PUT'])
-def update_state(state_id):
-    """ Method to update a state """
-    state = storage.get("State", state_id)
-    if state is None:
+def updates_state(state_id):
+    '''Updates a State object'''
+    all_states = storage.all("State").values()
+    state_obj = [obj.to_dict() for obj in all_states if obj.id == state_id]
+    if state_obj == []:
         abort(404)
     if not request.get_json():
-        abort(400, "Not a JSON")
-    for key, value in request.get_json().items():
-        if key not in ["id", "created_at", "updated_at"]:
-            setattr(state, key, value)
-    state.save()
-    return jsonify(state.to_dict()), 200
-
-
-if __name__ == "__main__":
-    pass
+        abort(400, 'Not a JSON')
+    state_obj[0]['name'] = request.json['name']
+    for obj in all_states:
+        if obj.id == state_id:
+            obj.name = request.json['name']
+    storage.save()
+    return jsonify(state_obj[0]), 20
